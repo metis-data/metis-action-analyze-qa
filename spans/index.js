e3f0e0f88ec71ff142ba078762abb8783d90903b
gideonshmila <gideons@metisdata.io>
2023-06-05 18:11:48 +0300

    update

--- spans/index.js
+++ spans/index.js
@@ -67,7 +67,7 @@ const makeSpan = async (query, queryType, plan, connection, prName) => {
     duration: 1,
     resource: {
       'app.tag.pr': prName,
-      'service.name': 'api-service',
+      'service.name': hostName,
       'metis.sdk.version': '67dee834d8b7eb0433640d45718759992dde0bb4',
       'metis.sdk.name': 'query-analysis',
       'telemetry.sdk.name': 'query-analysis',
@@ -82,35 +82,6 @@ const makeSpan = async (query, queryType, plan, connection, prName) => {
       'net.peer.port': 5432,
       'db.statement.metis.plan': parsedPlan,
     },
-  };
-
-  return {
-    parent_id: null,
-    name: queryType || 'REPL',
-    kind: 'SpanKind.CLIENT',
-    duration: 2,
-    start_time: startDate,
-    end_time: endDate,
-    attributes: {
-      'db.name': connection?.database,
-      'db.user': connection?.user,
-      'db.system': 'postgres',
-      'db.operation': queryType,
-      'db.statement': query,
-      'db.statement.metis': query + `/*traceparent=${traceId}-${span_id}*/''`,
-      'db.statement.metis.plan':
-        '{"Plan":{"Node Type":"Limit","Parallel Aware":false,"Startup Cost":8308.26,"Total Cost":438904.29,"Plan Rows":323,"Plan Width":40,"Output":["pass_id","passenger_id","booking_leg_id","seat","boarding_time","precheck","update_ts"],"Shared Hit Blocks":0,"Shared Read Blocks":0,"Shared Dirtied Blocks":0,"Shared Written Blocks":0,"Local Hit Blocks":0,"Local Read Blocks":0,"Local Dirtied Blocks":0,"Local Written Blocks":0,"Temp Read Blocks":0,"Temp Written Blocks":0,"I/O Read Time":0,"I/O Write Time":0,"Plans":[{"Node Type":"Gather","Parent Relationship":"Outer","Parallel Aware":false,"Startup Cost":8308.26,"Total Cost":438904.29,"Plan Rows":323,"Plan Width":40,"Output":["pass_id","passenger_id","booking_leg_id","seat","boarding_time","precheck","update_ts"],"Workers Planned":2,"Single Copy":false,"Shared Hit Blocks":0,"Shared Read Blocks":0,"Shared Dirtied Blocks":0,"Shared Written Blocks":0,"Local Hit Blocks":0,"Local Read Blocks":0,"Local Dirtied Blocks":0,"Local Written Blocks":0,"Temp Read Blocks":0,"Temp Written Blocks":0,"I/O Read Time":0,"I/O Write Time":0,"Plans":[{"Node Type":"Bitmap Heap Scan","Parent Relationship":"Outer","Parallel Aware":true,"Relation Name":"boarding_pass","Schema":"postgres_air","Alias":"boarding_pass","Startup Cost":7308.26,"Total Cost":437871.99,"Plan Rows":135,"Plan Width":40,"Output":["pass_id","passenger_id","booking_leg_id","seat","boarding_time","precheck","update_ts"],"Recheck Cond":"((boarding_pass.pass_id >= 10450000) AND (boarding_pass.pass_id <= 10800000))","Filter":"(boarding_pass.seat = \'20C\'::text)","Shared Hit Blocks":0,"Shared Read Blocks":0,"Shared Dirtied Blocks":0,"Shared Written Blocks":0,"Local Hit Blocks":0,"Local Read Blocks":0,"Local Dirtied Blocks":0,"Local Written Blocks":0,"Temp Read Blocks":0,"Temp Written Blocks":0,"I/O Read Time":0,"I/O Write Time":0,"Plans":[{"Node Type":"Bitmap Index Scan","Parent Relationship":"Outer","Parallel Aware":false,"Index Name":"boarding_pass_pkey","Startup Cost":0,"Total Cost":7308.18,"Plan Rows":348374,"Plan Width":0,"Index Cond":"((boarding_pass.pass_id >= 10450000) AND (boarding_pass.pass_id <= 10800000))","Shared Hit Blocks":0,"Shared Read Blocks":0,"Shared Dirtied Blocks":0,"Shared Written Blocks":0,"Local Hit Blocks":0,"Local Read Blocks":0,"Local Dirtied Blocks":0,"Local Written Blocks":0,"Temp Read Blocks":0,"Temp Written Blocks":0,"I/O Read Time":0,"I/O Write Time":0}]}]}]},"Planning":{"Shared Hit Blocks":0,"Shared Read Blocks":0,"Shared Dirtied Blocks":0,"Shared Written Blocks":0,"Local Hit Blocks":0,"Local Read Blocks":0,"Local Dirtied Blocks":0,"Local Written Blocks":0,"Temp Read Blocks":0,"Temp Written Blocks":0,"I/O Read Time":0,"I/O Write Time":0}}',
-      'net.peer.name': connection?.host,
-      'net.peer.ip': connection?.host,
-    },
-    status: {
-      status_code: 'UNSET',
-    },
-    context: {
-      span_id: span_id,
-      trace_id: traceId,
-    },
-    resource,
   };
 };
 
@@ -148,7 +119,7 @@ async function sendMultiSpans(url, apiKey, spans, prName) {
 }
 
 const sendSpans = async (metisApikey, queriesAndPlans, connection, metisExporterUrl, prName) => {
-  console.log(JSON.stringify(queriesAndPlans[0].plan));
+  console.log(JSON.stringify(prName));
   const spans = await Promise.all(
     queriesAndPlans?.map(async (item) => {
       return await makeSpan(item.query, 'select', item.plan, connection, prName);
